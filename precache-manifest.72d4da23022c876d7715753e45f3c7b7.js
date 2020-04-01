ad7f2dde5051339d6ece91b4bf75c9aa7997f1d2
Fora Bolsonaro <tchau.bozo@gmail.com>
2020-04-01 19:28:06 -0300

    Updates

---
+++ precache-manifest.72d4da23022c876d7715753e45f3c7b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089a3cffad732926f2fa1351a0e39e46",
+    "url": "/panelaco/index.html"
+  },
+  {
+    "revision": "45d49387b7c5171d91be",
+    "url": "/panelaco/static/css/main.49745c12.chunk.css"
+  },
+  {
+    "revision": "c7624127bf5e51e4a5d7",
+    "url": "/panelaco/static/js/2.f0327058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/panelaco/static/js/2.f0327058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d49387b7c5171d91be",
+    "url": "/panelaco/static/js/main.ecff4736.chunk.js"
+  },
+  {
+    "revision": "f1596b978b65ac9a98e7",
+    "url": "/panelaco/static/js/runtime-main.ac11b665.js"
+  },
+  {
+    "revision": "247b595129aa21e5b5c989fcf48cca1e",
+    "url": "/panelaco/static/media/fora-bolsonaro.247b5951.mp3"
+  },
+  {
+    "revision": "6f1160959223541cf2814295ca63075d",
+    "url": "/panelaco/static/media/logo.6f116095.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fora-bolsonaro/precache-manifest.b798f6481f6f0f43c163393701fd77bd.js"
+  "/panelaco/precache-manifest.72d4da23022c876d7715753e45f3c7b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fora-bolsonaro/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/panelaco/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
