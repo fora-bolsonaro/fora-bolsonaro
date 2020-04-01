0043b5096bde46f0b9af6f0b5694643d68db9848
Fora Bolsonaro <tchau.bozo@gmail.com>
2020-04-01 13:28:43 -0300

    Updates

---
+++ precache-manifest.f8d0fc9b84224918f8e11d5a4a54d0b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48fcbaff67584e73472b473a7d1d4a4f",
+    "url": "/panelaco/index.html"
+  },
+  {
+    "revision": "107eb129f1635ec6a659",
+    "url": "/panelaco/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a34108f9d7382b77629",
+    "url": "/panelaco/static/js/2.b7fff8e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/panelaco/static/js/2.b7fff8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107eb129f1635ec6a659",
+    "url": "/panelaco/static/js/main.e481181a.chunk.js"
+  },
+  {
+    "revision": "3244b033180fc50a0669",
+    "url": "/panelaco/static/js/runtime-main.b8afcd39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/panelaco/static/media/logo.5d5d9eef.svg"
+  }
+]);
